541efff668930b5d1fbec20684e5d7e51ef0aa30
Fernando_CR19 <fernandocr019@gmail.com>
2023-03-15 17:28:50 -0300

    Adiciona lógica do SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
 import { Login } from './components/Login';
 import { SignUp } from './components/SignUp';
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
 import './index.css';
 
 function App() {
+
+  // const API_KEY = process.env.API_KEY
+  const API_KEY = "384rj83f885z";
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+  const client = StreamChat.getInstance(API_KEY);
+
+  if (token) {
+    client
+      .connectUser(
+        {
+          id: cookies.get("userId"),
+          name: cookies.get("username"),
+          firstName: cookies.get("firstName"),
+          lastName: cookies.get("lastName"),
+          hashedPassword: cookies.get("hashedPassword"),
+        },
+        token
+      )
+      .then((user) => {
+        console.log("user", user)
+      });
+  };
+
   return (
     <div className="App">
       <SignUp />
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,27 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
+import Axios from "axios";
+import Cookies from "universal-cookie";
 
 export const SignUp = () => {
 
-    const [user, setUser] = useState(null)
+    const cookies = new Cookies();
 
-    const signUp = () => {}
+    const [user, setUser] = useState(null);
+
+    const signUp = () => {
+        Axios.post("http://localhost:5000/signup", user).then((res) => {
+            const { token, userId, firstName, lastName, username, hashedPassword } = res.data;
+
+            cookies.set("token", token);
+            cookies.set("userId", userId);
+            cookies.set("username", username);
+            cookies.set("firstName", firstName);
+            cookies.set("lastName", lastName);
+            cookies.set("hashedPassword", hashedPassword);
+        });
+    };
 
     return (
-        <div>
-            <label>Sign Up</label>
-            <input placeholder="First Name" onChange={(e) => {
-                setUser({ ...user, firstName: e.target.value })
+        <div className="signUp">
+            <label> Sign Up</label>
+            <input placeholder="First Name" onChange={(event) => {
+                setUser({ ...user, firstName: event.target.value });
             }} />
-            <input placeholder="Last Name" onChange={(e) => {
-                setUser({ ...user, lastName: e.target.value })
+            <input placeholder="Last Name" onChange={(event) => {
+                setUser({ ...user, lastName: event.target.value });
             }} />
-            <input placeholder="Username" onChange={(e) => {
-                setUser({ ...user, username: e.target.value })
+            <input placeholder="Username" onChange={(event) => {
+                setUser({ ...user, username: event.target.value });
             }} />
-            <input placeholder="Password" type="password" onChange={(e) => {
-                setUser({ ...user, password: e.target.value })
+            <input placeholder="Password" type="password" onChange={(event) => {
+                setUser({ ...user, password: event.target.value });
             }} />
             <button onClick={signUp}>Sign Up</button>
         </div>
-    )
+    );
 }
